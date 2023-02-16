86446bce410e07fe945f3ab97ea1dba6c05a5f5a
QVac` <vlad.nedaikash.cs.go.009@gmail.com>
2023-02-16 18:27:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message: %(asctime)s:%(levelname)s-%(message)s")
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+format="We have next logging message: "#
+"%(asctime)s:%(levelname)s-%(message)s"
+
+
+"""
+>>> 2+2
+5
+"""
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+def adder(*args, **kwargs):
+    result = 0
+    for _ in args:
+        if type(_) == int or type(_) == bool or type(_) == float:
+            result += _
+        else:
+            try:
+                result += float(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+            try:
+                result += int(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+    for _ in kwargs.values():
+        if type(_) == int or type(_) == bool or type(_) == float:
+            result += _
+        else:
+            try:
+                result += float(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+            try:
+                result += int(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+    return result
