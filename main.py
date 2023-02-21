87ccd3c0f3f2d8ee8cd424744188958bce839d50
QVac` <vlad.nedaikash.cs.go.009@gmail.com>
2023-02-21 16:39:12 +0200

    new2

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message: %(asctime)s:%(levelname)s-%(message)s")
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
-format="We have next logging message: "#
-"%(asctime)s:%(levelname)s-%(message)s"
+class cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.hunger = 70
+        self.energy = 60
+    def __str__(self):
+        return f"Cat {self.name}, age {self.age}"
 
-
-"""
->>> 2+2
-5
-"""
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
-
-def adder(*args, **kwargs):
-    result = 0
-    for _ in args:
-        if type(_) == int or type(_) == bool or type(_) == float:
-            result += _
+    def sleep(self):
+        if self.energy < 30:
+           print("I`m sleep")
+           self.energy += 50
+           self.hunger += 10
         else:
-            try:
-                result += float(_)
-                continue
-            except (ValueError, TypeError):
-                pass
-            try:
-                result += int(_)
-                continue
-            except (ValueError, TypeError):
-                pass
-    for _ in kwargs.values():
-        if type(_) == int or type(_) == bool or type(_) == float:
-            result += _
+            print("I want new toys!")
+    def eat(self):
+        if self.hunger < 30:
+            print("I`m eat")
+            self.hunger -= 30
+            self.energy += 10
+
+    def play(self):
+        if self.energy >= 20 and self.hunger < 80:
+            print("I`m play")
+            self.energy -= 20
+            self.hunger += 10
         else:
-            try:
-                result += float(_)
-                continue
-            except (ValueError, TypeError):
-                pass
-            try:
-                result += int(_)
-                continue
-            except (ValueError, TypeError):
-                pass
-    return result
+            print("I want sleep or eat")
+
+barsik = cat("Barsik", 6)
+
+print(barsik)
+
+barsik.play()
+
+barsik.eat()
+
+barsik.sleep()
